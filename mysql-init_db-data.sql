2cca9276e2cdbdcfcee1837881149f1b6e1fcd87
Elan Ruusamäe <glen@pld-linux.org>
2007-03-08 15:44:59 +0000

    - mysqldump mysql --no-create-info -O extended-insert=false | grep INSERT | sed -e 's,root,mysql,' | sed -e '/mysql/{p;s/localhost/$hostname/}'
    Changed files:
        mysql-init_db-data.sql -> 1.3

--- mysql-init_db-data.sql
+++ mysql-init_db-data.sql
@@ -1,4 +1,5 @@
 INSERT INTO `db` VALUES ('%','test','','Y','Y','Y','Y','Y','Y','N','Y','Y','Y','Y','Y','Y','Y','Y','N','N','Y','Y');
 INSERT INTO `db` VALUES ('%','test\\_%','','Y','Y','Y','Y','Y','Y','N','Y','Y','Y','Y','Y','Y','Y','Y','N','N','Y','Y');
 INSERT INTO `user` VALUES ('localhost','mysql','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0);
+INSERT INTO `user` VALUES ('$hostname','mysql','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0);
 INSERT INTO `user` VALUES ('localhost','','','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','','','','',0,0,0,0);
