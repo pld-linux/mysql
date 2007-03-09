6df746362a3a179481ce147fbde38873fd078b8a
Elan Ruusamäe <glen@pld-linux.org>
2007-03-09 13:38:30 +0000

    - use mysql_create_system_tables to initialize database
    Changed files:
        mysql-init_db-data.sql -> 1.5
        mysql-init_db.sql -> 1.6
        mysql.init -> 1.111

--- mysql-init_db-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `db` VALUES ('%','test','','Y','Y','Y','Y','Y','Y','N','Y','Y','Y','Y','Y','Y','Y','Y','N','N','Y','Y');
-INSERT INTO `db` VALUES ('%','test\\_%','','Y','Y','Y','Y','Y','Y','N','Y','Y','Y','Y','Y','Y','Y','Y','N','N','Y','Y');
-INSERT INTO `user` VALUES ('localhost','mysql','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0);
-INSERT INTO `user` VALUES ('$hostname','mysql','','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0);
-INSERT INTO `user` VALUES ('localhost','','','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','','','','',0,0,0,0);
-
-insert into user set host='localhost', user='mysql_logrotate', Reload_priv='Y';
