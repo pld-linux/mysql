b9c8dcd33857e9b0ee1f71263c538a675e07780c
cvs2git <feedback@pld-linux.org>
2010-12-16 18:25:16 +0000

    This commit was manufactured by cvs2git to create branch 'MYSQL_5_0'.
    Cherrypick from master 2010-12-16 18:25:16 UTC Elan Ruusamäe <glen@pld-linux.org> '- skip broken and unwanted patches':
        my.cnf -> 1.1
        mysql-client.conf -> 1.4
        percona.sh -> 1.7
    Cherrypick from master 2009-10-27 12:34:13 UTC Elan Ruusamäe <glen@pld-linux.org> '- fixing php-cli with php-mysql ext segfault:':
        mysql-fix-dummy-thread-race-condition.patch -> 1.1
    Cherrypick from master 2010-04-21 08:51:17 UTC Elan Ruusamäe <glen@pld-linux.org> '- skip empty lines with db cluster finding':
        mysql.init -> 1.143

---
+++ percona.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# updates percona patches
+# http://www.percona.com/docs/wiki/release:start
+
+version=release-5.1.53-11
+bzr_branch=lp:percona-server/$version
+branch=MYSQL_5_1
+
+filter_names() {
+	# mysql_dump_ignore_ct.patch is broken, therefore we skip
+	grep -v 'mysql_dump_ignore_ct.patch' | \
+	grep -v 'percona-support.patch' | \
+	grep -v 'mysqld_safe_syslog.patch' | \
+	grep -v 'mysql-test.diff'
+}
+
+filter_files() {
+	filterdiff -x '*/configure'
+}
+
+if [ -d $version ]; then
+	cd $version
+	bzr up
+	cd ..
+else
+	bzr branch $bzr_branch $version
+fi
+
+> .percona.spec
+> .patch.spec
+i=100
+for patch in $(cat $version/series | filter_names); do
+	file=mysql-$patch
+	cat $version/$patch | filter_files > $file
+
+	if [ -z "$(awk -vfile=$file -F/ '$2 == file{print}' CVS/Entries)" ]; then
+		cvs add $file
+		${branch:+cvs up -r $branch $file}
+	fi
+
+	printf "Patch%d:\t%s\n" $i %{name}-$patch >> .percona.spec
+	printf "%%patch%d -p1\n" $i >> .patch.spec
+	i=$((i+1))
+done
+
+# update PatchX section
+sed -i -e '
+/^# <percona patches/,/^# <\/percona>/ {
+	/^ <\/percona>/b
+	/^# <percona patches/ {
+		p # print header
+		r .percona.spec
+		a# </percona>
+	}
+	d
+}
+' mysql.spec
+
+# update %patchX section
+sed -i -e '
+/^# <percona %patches/,/^# <\/percona>/ {
+	/^ <\/percona>/b
+	/^# <percona %patches/ {
+		p # print header
+		r .patch.spec
+		a# </percona>
+	}
+	d
+}
+' mysql.spec
