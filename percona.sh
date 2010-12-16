a5142b1c9f21d0c016de316dcda9ced9f1b81cce
cvs2git <feedback@pld-linux.org>
2010-12-16 18:25:16 +0000

    This commit was manufactured by cvs2git to create tag 'auto-ac-mysql- 5_0_87-3'.
    Sprout from MYSQL_5_0 2009-10-28 13:31:12 UTC Elan Ruusamäe <glen@pld-linux.org> '- release 3'
    Cherrypick from MYSQL_5_0 2010-12-16 18:25:16 UTC cvs2git <feedback@pld-linux.org> 'This commit was manufactured by cvs2git to create branch 'MYSQL_5_0'.':
        mysql-fix-dummy-thread-race-condition.patch -> 1.1
    Cherrypick from master 2009-10-28 13:30:21 UTC Elan Ruusamäe <glen@pld-linux.org> '- sync two --bootstrap cmd args':
        mysql-client.conf -> 1.3
        mysql.init -> 1.137
        percona.sh -> 1.4
    Delete:
        mysql-split_buf_pool_mutex_fixed_optimistic_safe.patch

---
+++ percona.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# updates percona patches
+# http://www.percona.com/docs/wiki/release:start
+
+ver=5.0.86-b19
+patches=http://www.percona.com/mysql/$ver/patches
+series=$patches/series
+branch=MYSQL_5_0
+
+filter_names() {
+	grep -v 'mysqld_safe_syslog.patch'
+}
+
+filter_files() {
+	filterdiff -x '*/configure'
+}
+
+> .percona.spec
+> .patch.spec
+i=100
+for patch in $(wget -q -O - $series | filter_names); do
+	file=mysql-$patch
+	wget -nv $patches/$patch -O - | filter_files > $file
+
+	if [ -z "$(awk -vfile=$file -F/ '$2 == file{print}' CVS/Entries)" ]; then
+		cvs add $file
+		${branch:+cvs up -r $branch $file}
+	fi
+
+	printf "Patch%d:\t%s\n" $i %{name}-$patch >> .percona.spec
+	printf "%%patch%d -p1\n" $i >> .patch.spec
+	i=$((i+1))
+done
+
+# update PatchX section
+sed -i -e '
+/^# <percona patches/,/^# <\/percona>/ {
+	/^ <\/percona>/b
+	/^# <percona patches/ {
+		p # print header
+		r .percona.spec
+		a# </percona>
+	}
+	d
+}
+' mysql.spec
+
+# update %patchX section
+sed -i -e '
+/^# <percona %patches/,/^# <\/percona>/ {
+	/^ <\/percona>/b
+	/^# <percona %patches/ {
+		p # print header
+		r .patch.spec
+		a# </percona>
+	}
+	d
+}
+' mysql.spec
